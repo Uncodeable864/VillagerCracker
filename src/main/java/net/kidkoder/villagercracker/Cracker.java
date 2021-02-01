4ff57130703254b79f29db69bf20874ccec93181
Kid Koder <codermann09@gmail.com>
2021-02-01 15:48:50 -0600

    Initial commit(2)

---
+++ src/main/java/net/kidkoder/villagercracker/Cracker.java
@@ -0,0 +1,7 @@
+package net.kidkoder.villagercracker;
+
+public class Cracker {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/net/kidkoder/villagercracker/CrackerTest.java
@@ -0,0 +1,12 @@
+package net.kidkoder.villagercracker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CrackerTest {
+
+    @Test
+    public void main() {
+    }
+}
